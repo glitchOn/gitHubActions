4017435374e255fdf34d96737e7895783254d6fd
glitchOn <77013989+glitchOn@users.noreply.github.com>
2022-11-05 01:55:49 +0530

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,70 +1,10 @@
 var fs = require('fs');
 const path = require('path');
-
 const fastcsv = require('fast-csv');
 const currDir = path.join(__dirname + '/../data/');
-
-
-
 const core = require('@actions/core');
 const github = require('@actions/github');
-
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('userId');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-function createSTDResource() { 
-const config = {
-    headers: { Authorization: `Bearer ${token}` }
-};
-
-const bodyParameters = {
-   key: "value"
-};
-
-Axios.post( 
-  'http://localhost:8000/api/v1/get_token_payloads',
-  bodyParameters,
-  config
-).then(console.log).catch(console.log);
-
-}
-
-async function run() {
-    readdir(currDir).then((filenames) => {
-        filenames = filenames.filter(filtercsvFiles);
-      
-        for (let i = 0; i < filenames.length; i++) {
-          let currFilePath = currDir + filenames[i];
-      
-          //Use fast-csv to parse the files
-          let csvData = [];
-          fastcsv
-            .parseFile(currFilePath)
-            .on('data', (data) => {
-                    console.log(data);
-                    console.log(currFilePath);
-
-            })
-            .on('end', () => {
-              csvData.shift();
-
-            });
-        }
-      });
-
-
-  }
-  const readdir = (dirname) => {
+const readdir = (dirname) => {
     return new Promise((resolve, reject) => {
       fs.readdir(dirname, (error, filenames) => {
         if (error) {
@@ -78,9 +18,93 @@ async function run() {
   const filtercsvFiles = (filename) => {
     return filename.split('.')[1] == 'csv';
   };
+try {
+  // `who-to-greet` input defined in action metadata file
+  const userId = core.getInput('userId');
+  console.log(userId)
 
+  const password = core.getInput('userId');
+  console.log(password)
 
-
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
   
+//==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+// Step 1 : "Get JWT token
+//==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+/*fetch("https://myDomain/api/login", {
+    method: "POST",
+    body: JSON.stringify({ username: "username", password: "password" }),
+    headers: { "Content-type": "application/json" }
+  })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        return res.token;
+      })
+      .then((token) => {
+        fetchlist(token)
+        $('.clickMe').click(function(){
+          // alert(this.id);
+          fetchSingle(token)
+        });
+      });
+  
+  //==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+  // Step 2 : "Get Object list
+  //==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+  const fetchlist = (token) => {
+    const baseUrl = "https://myDomain/api/request/"
+      fetch(baseUrl, {
+        headers: {Authorization: `Bearer ${token}`}
+      })
+          .then((response) => {
+            if (response.status == 200) {
+              // alert ("json ok");
+              // console.log(response);
+              return response.json();
+            } else {
+              throw new Error("NETWORK RESPONSE ERROR");
+            }
+          })
+          .then((data) => {
+             console.log(data);
+          })
+          .catch(error => {
+            console.error(error)
+          });
+    } */
 
-  run();
+    run();
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+
+async function run() {
+    readdir(currDir).then((filenames) => {
+        filenames = filenames.filter(filtercsvFiles);
+      
+        for (let i = 0; i < filenames.length; i++) {
+          let currFilePath = currDir + filenames[i];
+      
+          //Use fast-csv to parse the files
+          let csvData = [];
+          fastcsv            
+            .parseFile(currFilePath,      { headers: true })
+            .on('data', (data) => {
+                    console.log(data);
+                    console.log(currFilePath);
+
+            })
+            .on('end', () => {
+              csvData.shift();
+
+
+            });
+        }
+      });
+
+
+  }
