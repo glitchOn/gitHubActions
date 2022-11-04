2af43444fd0fde9dd04a908713b0b329de3a1a04
Chandini kalluri <chandinikalluri@Chandinis-MacBook-Air.local>
2022-11-05 03:25:33 +0530

    1

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,43 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 994:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 594:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ }),
+
+/***/ 674:
+/***/ ((module) => {
+
+module.exports = eval("require")("fast-csv");
+
+
+/***/ }),
+
 /***/ 147:
 /***/ ((module) => {
 
 "use strict";
 module.exports = require("fs");
+
+/***/ }),
+
+/***/ 17:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");
 
 /***/ })
 
@@ -50,18 +82,111 @@ module.exports = require("fs");
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const fs = __nccwpck_require__(147);
+var fs = __nccwpck_require__(147);
+const path = __nccwpck_require__(17);
+const fastcsv = __nccwpck_require__(674);
+const currDir = __nccwpck_require__.ab + "data";
+const core = __nccwpck_require__(994);
+const github = __nccwpck_require__(594);
+const readdir = (dirname) => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(dirname, (error, filenames) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(filenames);
+        }
+      });
+    });
+  };
+  const filtercsvFiles = (filename) => {
+    return filename.split('.')[1] == 'csv';
+  };
+try {
+  // `who-to-greet` input defined in action metadata file
+  const userId = core.getInput('userId');
+  console.log(userId)
+
+  const password = core.getInput('userId');
+  console.log(password)
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  core.info(`The event payload: ${payload}`);
+  
+//==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+// Step 1 : "Get JWT token
+//==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
+/* fetch("https://myDomain/api/login", {
+    method: "POST",
+    body: JSON.stringify({ username: "username", password: "password" }),
+    headers: { "Content-type": "application/json" }
+  })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        return res.token;
+      })
+      .then((token) => {
+        });*/
+      
+ run();
+  /*
+  const fetchlist = (token) => {
+    const baseUrl = "https://myDomain/api/request/"
+      fetch(baseUrl, {
+        headers: {Authorization: `Bearer ${token}`}
+      })
+          .then((response) => {
+            if (response.status == 200) {
+              // alert ("json ok");
+              // console.log(response);
+              return response.json();
+            } else {
+              throw new Error("NETWORK RESPONSE ERROR");
+            }
+          })
+          .then((data) => {
+             console.log(data);
+          })
+          .catch(error => {
+            console.error(error)
+          });
+    } */
+
+   
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 
 async function run() {
-    fs.readFile("data/test.csv", "utf8", function (err, data) {
-        console.log(data);
+    readdir(__nccwpck_require__.ab + "data").then((filenames) => {
+        filenames = filenames.filter(filtercsvFiles);
+      
+        for (let i = 0; i < filenames.length; i++) {
+          let currFilePath = __nccwpck_require__.ab + "data/" + filenames[i];
+      
+          //Use fast-csv to parse the files
+          let csvData = [];
+          fastcsv            
+            .parseFile(currFilePath,      { headers: true })
+            .on('data', (data) => {
+                    console.log(data);
+                    console.log(currFilePath);
+
+            })
+            .on('end', () => {
+              csvData.shift();
+
+
+            });
+        }
       });
+
+
   }
-   
 
-  
-
-  run();
 })();
 
 module.exports = __webpack_exports__;
--- src/action.js
+++ src/action.js
@@ -28,7 +28,7 @@ try {
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  core.info(`The event payload: ${payload}`);
   
 //==================================≠≠≠≠≠====================≠≠≠≠≠≠≠≠
 // Step 1 : "Get JWT token
